ea881bb491d426948fb904973426deb53afe7e98
Roozbeh Khodadadeh <roozbeh.khd@powerdata.com>
2019-08-27 12:09:25 -0700

    emacs config initial

---
+++ init.el
@@ -0,0 +1,24 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+    (autoload 'ibuffer "ibuffer" "List buffers." t)
+(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t)
+ '(markdown-command "pandoc")
+ '(package-selected-packages (quote (org markdown-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(require 'org)
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+(setq org-default-notes-file (concat org-directory "/notes.org"))
